5c1d7bf40fef11059320c913a3cd3d0a303330af
JohnathanGG <yon,gabay@gmail.com>
2026-01-07 12:06:11 +0200

    Mark completed tasks

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,24 +1,43 @@
-import { Checkbox, List, ListItem } from "@mui/material";
-import { useAtomValue } from "jotai";
+import { Checkbox, List, ListItem, Typography } from "@mui/material";
+import { useAtom } from "jotai";
 
 import { tasks } from "../utils/atoms";
 import type { Task } from "../utils/types";
 
-function TaskItem({ task }: { task: Task }) {
-  return (
-    <ListItem>
-      <Checkbox />
-      {task.taskName}
-    </ListItem>
-  );
-}
-
 export default function ToDoList() {
-  const taskList = useAtomValue(tasks);
+  const [taskList, setTaskList] = useAtom(tasks);
+
+  const TaskItem = ({ task, index }: { task: Task; index: number }) => {
+    const handleChange = () => {
+      setTaskList((prevTasks) =>
+        prevTasks.map((t, i) =>
+          i === index ? { ...t, complete: !t.complete } : t
+        )
+      );
+    };
+
+    return (
+      <ListItem>
+        <Checkbox 
+          checked={task.complete}
+          onChange={handleChange}
+        />
+        <Typography
+          sx={{
+            color: task.complete ? 'darkgrey' : 'inherit',
+            textDecoration: task.complete ? 'line-through' : 'none',
+          }}
+        >
+          {task.taskName}
+        </Typography>
+      </ListItem>
+    );
+  };
+
   return (
-    <List>
+      <List>
       {taskList.map((taskItem: Task, index: number) => (
-        <TaskItem key={index} task={taskItem} />
+        <TaskItem key={index} task={taskItem} index={index} />
       ))}
     </List>
   );
