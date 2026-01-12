a8b0078a72c1677dcc61975342d6724cb69fe5d4
JohnathanGG <yon,gabay@gmail.com>
2026-01-12 13:23:35 +0200

    add  delete all

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-import NewTask from './components/NewTask'
-import ToDoList from './components/ToDoList'
-import DeleteButton from './components/DeleteButton'
+import NewTask from "./components/NewTask";
+import ToDoList from "./components/ToDoList";
+import DeleteButton from "./components/DeleteButton";
+import DeleteAllButton from "./components/DeleteAllButton";
 
-const theme = createTheme()
+const theme = createTheme();
 
 export default function App() {
-
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <NewTask/>
-      <ToDoList/>
-      <DeleteButton/>
+      <NewTask />
+      
+      <DeleteButton/> <DeleteAllButton />
+      <ToDoList />
     </ThemeProvider>
-  )
+  );
 }
---
+++ src/components/DeleteAllButton.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@mui/material";
+import {useSetAtom } from "jotai";
+
+import { tasks } from "../utils/atoms";
+import type { Task } from "../utils/types";
+
+export default function DeleteAllButton() {
+    const setTaskList = useSetAtom(tasks);
+
+    const DeleteTasks = () => {
+        setTaskList([]);
+    }
+    return (
+        <Button type="button" variant="contained" color="secondary" onClick={DeleteTasks}>
+        Delete All Tasks
+      </Button>
+    )
+}
