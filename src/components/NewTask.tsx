bf4c86df1a84298b7053441ce2ab2b6b45a853ab
JohnathanGG <yon,gabay@gmail.com>
2026-02-09 12:15:28 +0200

    add Formik and schema

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,39 +1,52 @@
 import { TextField, Button } from "@mui/material";
-import { useFormik } from "formik";
+import { Form, Formik } from "formik";
+import { z } from "zod";
+import { toFormikValidationSchema } from "zod-formik-adapter";
 
 import type { Task } from "../utils/types";
 import { useTasks } from "../hooks/useTasks";
 
+const validateTask = z.object({
+  taskName: z.string().min(3, "Task name must be at least 3 characters"),
+  complete: z.boolean(),
+});
+
 export default function NewTask() {
   const { setTasks } = useTasks();
 
-  const formik = useFormik({
-    initialValues: {
-      taskName: "",
-      complete: false,
-    },
-    onSubmit: (values) => {
-      const newTask: Task = {
-        taskName: values.taskName,
-        complete: values.complete,
-      };
-      setTasks((prevTasks) => [...prevTasks, newTask]);
-      formik.resetForm();
-    },
-  });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <TextField
-        label="Add New Task"
-        name="taskName"
-        value={formik.values.taskName}
-        onChange={formik.handleChange}
-        fullWidth
-        margin="normal"
-      />
-      <Button type="submit" variant="contained" color="primary">
-        Submit
-      </Button>
-    </form>
+    <Formik
+      initialValues={{
+        taskName: "",
+        complete: false,
+      }}
+      validationSchema={toFormikValidationSchema(validateTask)}
+      onSubmit={(values, { resetForm }) => {
+        const newTask: Task = {
+          taskName: values.taskName,
+          complete: values.complete,
+        };
+        setTasks((prevTasks) => [...prevTasks, newTask]);
+        resetForm();
+      }}
+    >
+      {(formik) => (
+        <Form>
+          <TextField
+            label="Add New Task"
+            name="taskName"
+            value={formik.values.taskName}
+            onChange={formik.handleChange}
+            error={formik.touched.taskName && Boolean(formik.errors.taskName)}
+            helperText={formik.touched.taskName && formik.errors.taskName}
+            fullWidth
+            margin="normal"
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 }
