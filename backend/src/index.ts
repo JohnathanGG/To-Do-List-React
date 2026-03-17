cfbfef7e64c6220d51d7e74c7d189ffb5bcb9017
JohnathanGG <yon,gabay@gmail.com>
2026-03-17 16:32:07 +0200

    update tasks

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.use("/api/users", taskRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- backend/src/routes/task.routes.ts
+++ backend/src/routes/task.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createTask, getTasks } from "../controllers/task.controller";
+import { createTask, getTasks, updateTask } from "../controllers/task.controller";
 
 
 const router = Router();
 
 router.get("/", getTasks);
 router.post("/", createTask);
+router.put("/:id", updateTask);
 
 export default router;
