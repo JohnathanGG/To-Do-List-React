cb648e3488c225a1e4ebc05ca41c743a453acbf6
JohnathanGG <yon,gabay@gmail.com>
2026-03-18 15:09:43 +0200

    connect to backend

--- frontend/src/hooks/useTasks.ts
+++ frontend/src/hooks/useTasks.ts
@@ -1,6 +1,9 @@
+import axios from "axios";
 import { useAtom, atom } from "jotai";
 
 import type Task from "../utils/types";
+
+const api = "/api/tasks";
 
 export const tasks = atom<Task[]>([]);
 export const useTasks = () => {
@@ -9,4 +12,28 @@ export const useTasks = () => {
     taskList,
     setTasks,
   };
+};
+
+export const getTasks = async () => {
+  const res = await axios.get(api);
+  const data = await res.data;
+  return data;
+};
+
+export const createTask = async (newTask: Task) => {
+  const res = await axios.post(api, { body: newTask });
+  const data = await res.data;
+  return data;
+};
+
+export const updateTask = async (newTask: Task, id: string) => {
+  const res = await axios.put(api + "/" + id, { body: newTask });
+  const data = await res.data;
+  return data;
+};
+
+export const deleteTasks = async (ids: string[]) => {
+  const res = await axios.delete(api, { data: { ids: ids } });
+  const data = await res.data;
+  return data;
 };
